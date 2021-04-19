85238e6530681acbb5dd3eed683fdcb0df63dba2
lennylouis <lenny.louis35@gmail.com>
2021-04-19 11:53:02 +0200

    add stuff to Enemy

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -1,13 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Enemy {
 
     private String name;
     private Attack attack;
+    private List<Equipment> stuff;
 
     public Enemy(String name, Attack attack) {
         this.name = name;
         this.attack = attack;
+        stuff = new ArrayList<>();
     }
 
     public String getName() {
